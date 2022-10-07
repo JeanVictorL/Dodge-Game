c3839a62c5ea922d34fcaaeabaaa812a63cdc821
Jean Victor <jeanvictor.lopes@gmail.com>
2022-10-07 03:16:27 -0300

    initial commit

---
+++ Assets/Scripts/FallingBlock.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class FallingBlock : MonoBehaviour
+{
+    public Vector2 speedMinMax;
+    float speed;
+    float visibleHeightThreshold;
+
+    void Start()
+    {
+        speed = Mathf.Lerp(speedMinMax.x, speedMinMax.y, Difficulty.GetDifficultyPercent());
+        visibleHeightThreshold = -Camera.main.orthographicSize - transform.localScale.y;
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
+
+        if (gameObject.transform.localPosition.y < visibleHeightThreshold)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 7f;
+    public event System.Action OnPlayerDeath;
+
+    float screenHalfWidthInWorldUnits;
+    void Start()
+    {
+        float halfPlayerWidth = transform.localScale.x / 2f;
+        screenHalfWidthInWorldUnits = Camera.main.aspect * Camera.main.orthographicSize + halfPlayerWidth;
+    }
+
+    void Update()
+    {
+        MovePlayer();
+        WrapArroundScreen();
+    }
+
+    void MovePlayer()
+    {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float move = inputX * speed * Time.deltaTime;
+        transform.Translate(Vector2.right * move);
+    }
+
+    void WrapArroundScreen()
+    {
+        if (transform.position.x < -screenHalfWidthInWorldUnits)
+        {
+            transform.position = new Vector2(screenHalfWidthInWorldUnits, transform.position.y);
+        }
+        else if (transform.position.x > screenHalfWidthInWorldUnits)
+        {
+            transform.position = new Vector2(-screenHalfWidthInWorldUnits, transform.position.y);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Falling Block")
+        {
+            if (OnPlayerDeath != null)
+            {
+                OnPlayerDeath();
+            }
+            Destroy(gameObject);
+        }
+    }
+}
